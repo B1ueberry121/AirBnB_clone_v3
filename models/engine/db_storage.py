c0d615d01a41ba49d71e41d66042bbabe1054de5
GabsMermaid <g_martinez_rodz@gmail.com>
2022-02-04 19:59:45 +0000

    Updated db & file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,20 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def get(self, cls, id):
+        """Retrieve one object"""
+        obj_dict = self.all(cls)
+        for key in obj_dict.values():
+            if key.id == id:
+                return key
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects in your storage"""
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
