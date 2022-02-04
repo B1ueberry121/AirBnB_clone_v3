48d5e4fd03979a1b9d8db594b6ece54b668695a1
B1ueberry121 <christianlaboy839@gmail.com>
2022-02-04 21:24:07 +0000

    task 4 first commit all except views/__init__.py

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Module starts an API '''
+
+# Imports Flask task
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+@app.teardown_appcontext
+def close_storage(error):
+    ''' Calls the close method from storage '''
+    storage.close()
+
+if __name__ == "__main__":
+    """ Only executes as main """
+    api_host = getenv("HBNB_API_HOST")
+    if not api_host:
+        api_host = "0.0.0.0"
+    api_port = getenv("HBNB_API_PORT")
+    if not api_port:
+        api_port = "5000"
+    app.run(host=api_host, port=api_port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' 
+    This module handles some routing options for the flask app
+'''
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def get_status():
+    """ Tests the route status returning a basic JSON query """
+    dic = {'status': 'OK'}
+    return jsonify(dic)
