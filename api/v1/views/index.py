8f8e0bc0c57de69cd4c03716339bc5ddae37911e
B1ueberry121 <christianlaboy839@gmail.com>
2022-02-05 23:42:25 +0000

    adding new imports and using the storage counter in the new route stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,31 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     """ Tests the route status returning a basic JSON query """
     dic = {'status': 'OK'}
+    return jsonify(dic)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """ Gets the count of all obj created """
+    dic = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+    }
     return jsonify(dic)
