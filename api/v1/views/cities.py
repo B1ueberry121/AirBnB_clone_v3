32a8671ded794cf8e204502fde3934f48eef6d32
B1ueberry121 <christianlaboy839@gmail.com>
2022-02-07 01:48:13 +0000

    adding city_id=None in get_city and changing order

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,34 +9,6 @@ from models.city import City
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>', methods=['GET', 'PUT', 'DELETE'],
-                 strict_slashes=False)
-def get_city():
-    """ Handles HTTP requests of all city objects of a state object"""
-
-    city = storage.get(City, city_id)
-    if city is None:
-        abort(404)
-
-    if request.method == 'DELETE':
-        storage.delete(city)
-        storage.save()
-        return jsonify({}), 200
-
-    if request.method == 'PUT':
-        data = request.get_json()
-        if not data:
-            return Response("Not a JSON", 400)
-        data['id'] = city.id
-        data['created_at'] = city.created_at
-        data['state_id'] = city.state_id
-        city.__init__(**data)
-        city.save()
-        return jsonify(city.to_dict()), 200
-
-    return jsonify(city.to_dict())
-
-
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'],
                  strict_slashes=False)
 def get_cities(state_id=None):
@@ -63,3 +35,31 @@ def get_cities(state_id=None):
         if city.state_id == state.id:
             cities.append(cities.to_dict())
     return jsonify(cities)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET', 'PUT', 'DELETE'],
+                 strict_slashes=False)
+def get_city(city_id=None):
+    """ Handles HTTP requests of all city objects of a state object"""
+
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+
+    if request.method == 'DELETE':
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            return Response("Not a JSON", 400)
+        data['id'] = city.id
+        data['created_at'] = city.created_at
+        data['state_id'] = city.state_id
+        city.__init__(**data)
+        city.save()
+        return jsonify(city.to_dict()), 200
+
+    return jsonify(city.to_dict())
